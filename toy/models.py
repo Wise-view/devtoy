a527a70d759d516bcec67063d5f4bb44416dd19e
Fotsingboris <fotsingtchoupe1@gmail.com>
2022-06-07 01:21:28 +0100

    fix: i changed the names of file in the templates folder from forms to toy and i also changed the name of the field in the toy.html file and created the urls.py file for the app toy

--- toy/models.py
+++ toy/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class Forms(models.Model):
-    album_name = models.CharField(max_length=35, null=False, unique=True)
-    artist_name = models.CharField(max_length=35, null=False, unique=True)
-    album_picture = models.ImageField(upload_to="media/")
+class Toys(models.Model):
+    toy_name = models.CharField(max_length=35, null=False, unique=True)
+    toy_picture = models.ImageField(upload_to="media/")
 
     def __str__(self): 
-        return self.artist_name
+        return self.toy_name

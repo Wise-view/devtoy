e27ad5fde1bcac5d25d9c06dd5324b12fc9355c9
Steve Yonkeu <yokwejuste@yahoo.com>
2022-06-09 15:21:42 +0100

    feat: reverted the previous commit

--- devtoy/test_app/views.py
+++
@@ -1,9 +0,0 @@
-import re
-from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse
-
-
-# Create your views here.
-#I'll create a view called homepage.
-def homepage(request):
-    return HttpResponse("Hello <strong>Wise View<strong/>")
